b527d3f3845b7b4f48370d045de11a83ec7b7f5d
Capmu <p.kanawat.capmu@gmail.com>
2023-06-02 01:32:29 +0700

    getting data from db.json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { OrderDialogComponent } from './order-dialog/order-dialog.component';
+import { MatTableDataSource } from '@angular/material/table';
+import { BackendService } from './services/backend.service';
 
 @Component({
   selector: 'app-root',
@@ -9,10 +11,28 @@ import { OrderDialogComponent } from './order-dialog/order-dialog.component';
 })
 export class AppComponent {
   title = 'test-ccpdl';
+  dataSource!: MatTableDataSource<any>;
 
-  constructor(private dialog: MatDialog){}
+  constructor(private dialog: MatDialog, private backendService: BackendService) { }
+
+  ngOnInit(): void {
+    this.getAllOrders()
+  }
+
+  getAllOrders() {
+    this.backendService.getOrder()
+      .subscribe({
+        next: (response) => {
+          this.dataSource = new MatTableDataSource(response)
+          console.log(this.dataSource.data) //to check the fetched data
+        },
+        error: () => {
+          alert('Error while getting orders')
+        }
+      })
+  }
 
   openOrderDialog() {
-    this.dialog.open(OrderDialogComponent, {width: '30%'});
+    this.dialog.open(OrderDialogComponent, { width: '30%' });
   }
 }
